f8f263b81a6b59f2847ae752d52666b7eede84d8
ytw <yqqq2015@gmail.com>
2021-03-12 00:22:37 +0800

    添加BaseActivity、BaseFragment

--- common/src/main/java/com/android/common/AbsApplication.kt
+++ common/src/main/java/com/android/common/AbsApplication.kt
@@ -1,7 +1,10 @@
 package com.android.common
 
+import android.app.Activity
 import android.app.Application
 import android.content.Context
+import android.os.Bundle
+import com.android.base.activity.ActivityManager
 import com.android.network.base.INetworkRequiredInfo
 import com.android.network.base.NetworkApi
 
@@ -11,10 +14,39 @@ abstract class AbsApplication : Application() {
         this.applicationContext
     }
 
+    private val _activityLifecycleCallback = object : ActivityLifecycleCallbacks {
+
+        override fun onActivityCreated(activity: Activity, savedInstanceState: Bundle?) {
+            ActivityManager.getInstance().add(activity)
+        }
+
+        override fun onActivityStarted(activity: Activity) {
+        }
+
+        override fun onActivityResumed(activity: Activity) {
+        }
+
+        override fun onActivityPaused(activity: Activity) {
+        }
+
+        override fun onActivityStopped(activity: Activity) {
+        }
+
+        override fun onActivitySaveInstanceState(activity: Activity, outState: Bundle) {
+        }
+
+        override fun onActivityDestroyed(activity: Activity) {
+            ActivityManager.getInstance().remove()
+        }
+
+    }
+
     override fun onCreate() {
         super.onCreate()
         instance = this
         NetworkApi.initNetwork(networkInfo())
+
+        registerActivityLifecycleCallbacks(_activityLifecycleCallback)
     }
 
     abstract fun networkInfo(): INetworkRequiredInfo
